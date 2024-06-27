5ecdcd1a47276661ea99215e809933b13383a079
unknown <you@example.com>
2024-06-27 11:07:32 +0200

    first commit

---
+++ js/bonjour.js
@@ -0,0 +1,5 @@
+let prenom
+let nom
+prompt("nom")
+ window.prompt("prénom");
+ prompt("Nom de l'utilisateur  Prénom de l'utilisateur");
---
+++ js/comparaison.js
@@ -0,0 +1,19 @@
+let nb1 = prompt("donne moi un nombre");
+let nb2 = prompt("donne moi un nombre");
+
+console.log("nb1 =" + nb1);
+console.log("nb2 =" + nb2);
+
+if(nb1 > nb2){
+
+    alert(nb1 + " est superieur a " + nb2); 
+
+} else if(nb1 < nb2){
+
+    alert(nb1 + " est inferieur a " + nb2);
+
+} else if( nb1 == nb2){
+
+    alert( nb1 + " est égale a " + nb2 );
+    
+}
